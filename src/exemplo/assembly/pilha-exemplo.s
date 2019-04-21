c62569630d2b13d20a27ed3f0402dcd0afc08526
eduardoparducci <eduardo.parducci@outlook.com>
2019-04-21 15:55:46 -0300

    Update pilha-exemplo.s

--- src/exemplo/assembly/pilha-exemplo.s
+++ src/exemplo/assembly/pilha-exemplo.s
@@ -1,6 +1,6 @@
-	  ;; Código exemplo de uso de pilha no ARM Assembly
-    mov		sp, #0x200
-	  mov		r0, #1  
-	  stmfd	sp!, {r0}
-	  mov		r0, #0
-	  ldmfd	sp!, {r0}
+	;; Código exemplo de uso de pilha no ARM Assembly
+	mov	sp, #0x200
+	mov	r0, #1  
+	stmfd	sp!, {r0}
+	mov	r0, #0
+	ldmfd	sp!, {r0}
