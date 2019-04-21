131d3dc60e595cb9cf69d1771cd0cc7f14c2e23d
eduardoparducci <eduardo.parducci@outlook.com>
2019-04-21 15:55:09 -0300

    Update soma-pilha-pilha.s

--- src/exemplo/assembly/soma-pilha-pilha.s
+++ src/exemplo/assembly/soma-pilha-pilha.s
@@ -1,13 +1,13 @@
-		;;		configuraçao pilha
-		mov		sp, #0x200
+	;;	configuraçao pilha
+	mov	sp, #0x200
 		
-		;;		empilhando valores arbitrarios (5 e 3)
-		mov		r0, #3
-		stmfd	sp!, {r0}
-		mov		r0, #5
-		stmfd	sp!, {r0}
+	;;	empilhando valores arbitrarios (5 e 3)
+	mov	r0, #3
+	stmfd	sp!, {r0}
+	mov	r0, #5
+	stmfd	sp!, {r0}
 		
-		;;		realizando a operaçao
-		ldmfd	sp!, {r1}
-		ldmfd	sp!, {r2}
-		add		r0, r1, r2
+	;;		realizando a operaçao
+	ldmfd	sp!, {r1}
+	ldmfd	sp!, {r2}
+	add	r0, r1, r2
